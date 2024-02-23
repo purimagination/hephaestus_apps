a917b56efa9e861f05b95ba11a3bb67da58c1b2c
0nhc <thefoxfoxfox@outlook.com>
2024-02-23 14:38:29 +0800

    init

---
+++ scripts/move_to_point.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+import rospy
+import moveit_commander
+from moveit_commander.conversions import pose_to_list
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+import math
+import sys
+
+def dist(p, q):
+    return math.sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
+
+def all_close(goal, actual, tolerance):
+    """
+    Convenience method for testing if the values in two lists are within a tolerance of each other.
+    For Pose and PoseStamped inputs, the angle between the two quaternions is compared (the angle
+    between the identical orientations q and -q is calculated correctly).
+    @param: goal       A list of floats, a Pose or a PoseStamped
+    @param: actual     A list of floats, a Pose or a PoseStamped
+    @param: tolerance  A float
+    @returns: bool
+    """
+    if type(goal) is list:
+        for index in range(len(goal)):
+            if abs(actual[index] - goal[index]) > tolerance:
+                return False
+
+    elif type(goal) is geometry_msgs.msg.PoseStamped:
+        return all_close(goal.pose, actual.pose, tolerance)
+
+    elif type(goal) is geometry_msgs.msg.Pose:
+        x0, y0, z0, qx0, qy0, qz0, qw0 = pose_to_list(actual)
+        x1, y1, z1, qx1, qy1, qz1, qw1 = pose_to_list(goal)
+        # Euclidean distance
+        d = dist((x1, y1, z1), (x0, y0, z0))
+        # phi = angle between orientations
+        cos_phi_half = math.fabs(qx0 * qx1 + qy0 * qy1 + qz0 * qz1 + qw0 * qw1)
+        return d <= tolerance and cos_phi_half >= math.cos(tolerance / 2.0)
+
+    return True
+
+class MoveGroupPythonInterfaceTutorial(object):
+    def __init__(self) -> None:
+        super(MoveGroupPythonInterfaceTutorial, self).__init__()
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
+        robot = moveit_commander.RobotCommander()
+        scene = moveit_commander.PlanningSceneInterface()
+        group_name = "hephaestus_arm_group"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+        planning_frame = move_group.get_planning_frame()
+        print("============ Planning frame: %s" % planning_frame)
+        eef_link = move_group.get_end_effector_link()
+        print("============ End effector link: %s" % eef_link)
+        group_names = robot.get_group_names()
+        print("============ Available Planning Groups:", robot.get_group_names())
+        print("============ Printing robot state")
+        print(robot.get_current_state())
+        print("")
+
+        self.robot = robot
+        self.scene = scene
+        self.move_group = move_group
+        self.planning_frame = planning_frame
+        self.eef_link = eef_link
+        self.group_names = group_names
+
+    def go_to_joint_state(self):
+        move_group = self.move_group
+        joint_goal = move_group.get_current_joint_values()
+        joint_goal[0] = 0
+        joint_goal[1] = -math.tau / 8
+        joint_goal[2] = 0
+        joint_goal[3] = -math.tau / 4
+        joint_goal[4] = 0
+        joint_goal[5] = math.tau / 6  # 1/6 of a turn
+        move_group.go(joint_goal, wait=True)
+        move_group.stop()
+        current_joints = move_group.get_current_joint_values()
+        return all_close(joint_goal, current_joints, 0.01)
+    
+def main():
+    try:
+        print("")
+        print("----------------------------------------------------------")
+        print("Welcome to the MoveIt MoveGroup Python Interface Tutorial")
+        print("----------------------------------------------------------")
+        print("Press Ctrl-D to exit at any time")
+        print("")
+        input(
+            "============ Press `Enter` to begin the tutorial by setting up the moveit_commander ..."
+        )
+        tutorial = MoveGroupPythonInterfaceTutorial()
+
+        input(
+            "============ Press `Enter` to execute a movement using a joint state goal ..."
+        )
+        tutorial.go_to_joint_state()
+
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+    
+if __name__ == "__main__":
+    main()
